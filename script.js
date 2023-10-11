1673b25a2256d349899d4ea47a6b3aadb3553b37
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-10-11 19:37:42 +0300

    add terms

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-var map = L.map("map", {
-  center: [50.45179, 30.52585],
-  zoom: 17,
-});
+if (document.querySelector("#map")) {
+  var map = L.map("map", {
+    center: [50.45179, 30.52585],
+    zoom: 17,
+  });
+  var tiles = new L.tileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+  ).addTo(map);
 
-var tiles = new L.tileLayer(
-  "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-).addTo(map);
+  var marker = L.marker([50.45179, 30.52585], {
+    title: "IHub",
+  });
 
-var marker = L.marker([50.45179, 30.52585], {
-  title: "IHub",
-});
+  marker.addTo(map);
 
-marker.addTo(map);
+  document.querySelector(".program__list").addEventListener("click", (e) => {
+    const item = e.target.closest(".program__item");
 
-document.querySelector(".program__list").addEventListener("click", (e) => {
-  const item = e.target.closest(".program__item");
+    if (!item) return;
 
-  if (!item) return;
-
-  item.classList.toggle("program__item--open");
-});
+    item.classList.toggle("program__item--open");
+  });
+}
 
 var map2 = L.map("map2", {
   center: [50.45179, 30.52585],
