cda46649fe5a82dcb92945de1b2927ec900e3be3
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-10-12 15:31:53 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// document.querySelectorAll(".btn__border").forEach((el) =>
+//   el.addEventListener("click", () => {
+//     console.log("here");
+//     fetch(
+//       "https://graph.facebook.com/v18.0/1413267286204643/events?access_token=EAABZBrK85TNcBOzae031rNWlb3jMkFhQ1ZC4rtvFEIdZAxzqyOZA7NjLS3rLZBjGmijzyBcN6ZA7BNMQCj6JItvZAsmr0R6WfeKkdxCRYGJOYyqAlZA2qPDUiJQa66SBGQD0vv6CjLIqjuwlLWo3PtGHVZAanRG13FaK2VhcBZCwrbLD5RlytQN6KqMaoAIvfVZAufbkgZDZD",
+//       {
+//         method: "POST",
+//         body: JSON.stringify({
+//           data: [
+//             {
+//               event_name: "Click",
+//               event_time: Date.now(),
+//               action_source: "website",
+//               event_source_url: "https://teplo.netlify.app/",
+//               user_data: {
+//                 // "em": [
+//                 //     "7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068"
+//                 // ],
+//                 ph: [null],
+//               },
+//             },
+//           ],
+//         }),
+//       }
+//     )
+//       .then((res) => res.json())
+//       .then((data) => console.log(data));
+//   })
+// );
+
 if (document.querySelector("#map")) {
   var map = L.map("map", {
     center: [50.45179, 30.52585],
